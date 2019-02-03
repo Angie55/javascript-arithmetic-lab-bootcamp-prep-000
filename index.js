28fc033e8442b324af338b54bfaa3ae58144059d
Angela Stead <angie_stead55@yahoo.com>
2019-02-03 17:18:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,14 +28,7 @@ function dec(n)
   return n - 1;
 }
 
-function makeInt (n){
-var parsed = parseInt(n);
-return parsed;
-}
 
-function makeInt(n) {
-  return parseInt(n, 10);
-}
 
 function makeInt(n) 
 {
